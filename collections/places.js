70b81130fc74ca3a288de8d0dc133ff2af754653
wduntak <wduntak@gmail.com>
2015-03-05 16:56:12 -0500

    Get cordova and dara fetching from google api

--- collections/places.js
+++ collections/places.js
@@ -3,7 +3,8 @@ Places = new Mongo.Collection('places');
 Meteor.methods({
 	'fetchNearbyLocations': function(coords){
 		if(Meteor.isServer) {
-			request = HTTP.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + coords.latitude + "," + coords.longitude + "&radius=500&types=food&key=AIzaSyA0edY1nU9H_PXmILX6Q7z4uJu-SRphrYg")
+			request = HTTP.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + coords.latitude + "," + coords.longitude + "&radius=100&types=food&key=AIzaSyBW9fGuGpQDdo6_3J5LejtT0TLEsRoEVis&sensor=true")
+			console.log(request)
 			_(request.data.results).each(function(place){
 				_.extend(place, {loc: {type: "Point", coordinates: [place.geometry.location.lng, place.geometry.location.lat]}})
 				Places.upsert({id: place.id}, {$set: place})
